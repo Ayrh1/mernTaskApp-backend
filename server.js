d3939f7410cb7c39b1227333e15dafdee24b4702
ayrh90-fZNzxW <ayrh90@icloud.com>
2024-02-26 22:02:32 -0800

    cors fix

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express(); 
 
+const corsOptions = {
+    origin: 'https://classy-daffodil-dcfa9f.netlify.app',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(Cors());
+app.use(Cors(corsOptions));
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
